1357ef716a3fd0d4e53e429c5285022f44a41e90
Edmund Mergl <E.Mergl@bawue.de>
1996-11-24 11:04:43 +0000

    Import of MERGL/pg95perl5-1.4.2 from CPAN.
    gitpan-cpan-distribution: pg95perl5
    gitpan-cpan-version:      1.4.2
    gitpan-cpan-path:         MERGL/pg95perl5-1.4.2.tar.gz
    gitpan-cpan-author:       MERGL
    gitpan-cpan-maturity:     released

--- Pg.pm
+++ Pg.pm
@@ -1,6 +1,6 @@
 #-------------------------------------------------------
 #
-# $Id: Pg.pm,v 2.2 1996/11/24 09:21:12 mergl Exp $
+# $Id: Pg.pm,v 2.3 1996/11/24 10:52:46 mergl Exp $
 #
 #-------------------------------------------------------
 
@@ -78,7 +78,7 @@ require 5.003;
 	PGRES_InvalidOid
 );
 
-$VERSION = '1.4';
+$VERSION = '1.4.2';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
@@ -142,22 +142,18 @@ Pg - Perl extension for Postgres95
 
 new style:
 
-use Pg;
-C<
-$conn = Pg::new('', '', '', '', 'template1');
-$result = $conn-&gtexec("create database test");
->
+    use Pg;
+    $conn = Pg::new('', '', '', '', 'template1');
+    $result = $conn->exec("create database test");
 
 
 you may also use the old style:
 
-use Pg;
-C<
-$conn = PQsetdb('', '', '', '', template1);
-$result = PQexec($conn, "create database test");
-PQclear($result);
-PQfinish($conn);
->
+    use Pg;
+    $conn = PQsetdb('', '', '', '', template1);
+    $result = PQexec($conn, "create database test");
+    PQclear($result);
+    PQfinish($conn);
 
 
 =head1 DESCRIPTION
@@ -171,7 +167,7 @@ examples look at the Libpq applications in
 ../src/test/examples and ../src/test/regress. 
 
 You have the choice between the old C-style and a 
-new more Perl-ish style. The old style has the 
+new, more Perl-ish style. The old style has the 
 benefit, that existing Libpq applications can be 
 ported to perl just by prepending every variable 
 with a '$'. The new style uses class packages and 
@@ -179,6 +175,16 @@ might be more familiar for C++-programmers.
 
 
 =head1 GUIDELINES
+
+=head2 new style
+
+The new style uses blessed references as objects. 
+After creating a new connection or result object, 
+the relevant Libpq functions serve as virtual methods. 
+One benefit of the new style: you do not have to care 
+about freeing the connection- and result-structures. 
+Perl calls the destructor whenever the last reference 
+to an object goes away. 
 
 =head2 old style
 
@@ -190,35 +196,23 @@ constants start with 'PGRES_' (e.g. PGRES_CONNECTION_OK).
 There are two functions, which allocate memory, that has 
 to be freed by the user: 
 
-	PQsetdb, use PQfinish to free memory.
-	PQexec,  use PQclear to free memory.
+    PQsetdb, use PQfinish to free memory.
+    PQexec,  use PQclear to free memory.
 
 
 Pg.pm contains one convenience function: doQuery. It fills a
 two-dimensional array with the result of your query. Usage:
 
-C<
-Pg::doQuery($conn, "select attr1, attr2 from tbl", \@ary);
+    Pg::doQuery($conn, "select attr1, attr2 from tbl", \@ary);
 
-for $i ( 0 .. $#ary ) {
-    for $j ( 0 .. $#{$ary[$i]} ) {
-        print "$ary[$i][$j]\t";
+    for $i ( 0 .. $#ary ) {
+        for $j ( 0 .. $#{$ary[$i]} ) {
+            print "$ary[$i][$j]\t";
+        }
+        print "\n";
     }
-    print "\n";
-}
->
 
 Notice the inner loop !
-
-=head2 new style
-
-The new style uses blessed references as objects. 
-After creating a new connection or result object, 
-the relevant Libpq functions serve as virtual methods. 
-One benefit of the new style: you do not have to care 
-about freeing the connection- and result-structures. 
-Perl calls the destructor whenever the last reference 
-to an object goes away. 
 
 
 =head1 CAVEATS
@@ -242,67 +236,68 @@ database. In Libpq a connection is represented by a structure called
 PGconn. Using the appropriate methods you can access almost all 
 fields of this structure.
 
-B<$conn = Pg::new($pghost, $pgport, $pgoptions, $pgtty, $dbname)>
+    $conn = Pg::new($pghost, $pgport, $pgoptions, $pgtty, $dbname)
 
 Opens a new connection to the backend. You may use an empty string for
 any argument, in which case first the environment is checked and then 
 hardcoded defaults are used. The connection identifier $conn ( a pointer 
 to the PGconn structure ) must be used in subsequent commands for unique 
-identification. Before using $conn you should call $conn-&gtstatus to ensure, 
+identification. Before using $conn you should call $conn->status to ensure, 
 that the connection was properly made. Use the methods below to access 
 the contents of the PGconn structure.
 
-B<PQfinish($conn)>
+    PQfinish($conn)
 
 Old style only !
 Closes the connection to the backend and frees all memory. 
 
-B<$conn-&gtreset>
+    $conn->reset
 
 Resets the communication port with the backend and tries
 to establish a new connection.
 
-B<$dbName = $conn-&gtdb>
+    $dbName = $conn->db
 
 Returns the database name of the connection.
 
-B<$pghost = $conn-&gthost>
+    $pghost = $conn->host
 
 Returns the host name of the connection.
 
-B<$pgoptions = $conn-&gtoptions>
+    $pgoptions = $conn->options
 
 Returns the options used in the connection.
 
-B<$pgport = $conn-&gtport>
+    $pgport = $conn->port
 
 Returns the port of the connection.
 
-B<$pgtty = $conn-&gttty>
+    $pgtty = $conn->tty
 
 Returns the tty of the connection.
 
-B<$status = $conn-&gtstatus>
+    $status = $conn->status
 
 Returns the status of the connection. For comparing the status 
 you may use the following constants: 
- - PGRES_CONNECTION_OK
- - PGRES_CONNECTION_BAD
 
-B<$errorMessage = $conn-&gterrorMessage>
+  - PGRES_CONNECTION_OK
+  - PGRES_CONNECTION_BAD
+
+    $errorMessage = $conn->errorMessage
 
 Returns the last error message associated with this connection.
 
-B<$conn-&gttrace(debug_port)>
+    $conn->trace(debug_port)
 
 Messages passed between frontend and backend are echoed to the 
 debug_port file stream. 
 
-B<$conn-&gtuntrace>
+    $conn->untrace
 
 Disables tracing. 
 
-B<$result = $conn-&gtexec($query)>
+    $result = $conn->exec($query)
 
 Submits a query to the backend. The return value is a pointer to 
 the PGresult structure, which contains the complete query-result 
@@ -313,7 +308,7 @@ structure has to be freed using PQfree. Before using $result you
 should call resultStatus to ensure, that the query was 
 properly executed. 
 
-B<$ret = $conn-&gtgetline($string, $length)>
+    $ret = $conn->getline($string, $length)
 
 Reads a string up to $length - 1 characters from the backend. 
 getline returns EOF at EOF, 0 if the entire line has been read, 
@@ -321,13 +316,13 @@ and 1 if the buffer is full. If a line consists of the two
 characters "\." the backend has finished sending the results of 
 the copy command. 
 
-B<$conn-&gtputline($string)>
+    $conn->putline($string)
 
 Sends a string to the backend. The application must explicitly 
 send the two characters "\." to indicate to the backend that 
 it has finished sending its data. 
 
-B<$ret = $conn-&gtendcopy>
+    $ret = $conn->endcopy
 
 This function waits  until the backend has finished the copy. 
 It should either be issued when the last string has been sent 
@@ -335,7 +330,7 @@ to  the  backend  using  putline or when the last string has
 been received from the backend using getline. endcopy returns 
 0 on success, nonzero otherwise. 
 
-B<($table, $pid) = $conn-&gtnotifies>
+    ($table, $pid) = $conn->notifies
 
 Checks for asynchronous notifications. This functions differs from 
 the C-counterpart which returns a pointer to a new allocated structure, 
@@ -352,73 +347,74 @@ methods you can access almost all fields of this structure.
 
 Use the functions below to access the contents of the PGresult structure.
 
-B<$ntups = $result-&gtntuples
+    $ntups = $result->ntuples
 
 Returns the number of tuples in the query result.
 
-B<$nfields = $result-&gtnfields>
+    $nfields = $result->nfields
 
 Returns the number of fields in the query result.
 
-B<$fname = $result-&gtfname($field_num)>
+    $fname = $result->fname($field_num)
 
 Returns the field name associated with the given field number. 
 
-B<$fnumber = $result-&gtfnumber($field_name)>
+    $fnumber = $result->fnumber($field_name)
 
 Returns the field number associated with the given field name. 
 
-B<$ftype = $result-&gtftype($field_num)>
+    $ftype = $result->ftype($field_num)
 
 Returns the oid of the type of the given field number. 
 
-B<$fsize = $result-&gtfsize($field_num)>
+    $fsize = $result->fsize($field_num)
 
 Returns the size in bytes of the type of the given field number. 
 It returns -1 if the field has a variable length.
 
-B<$value = $result-&gtgetvalue($tup_num, $field_num)>
+    $value = $result->getvalue($tup_num, $field_num)
 
 Returns the value of the given tuple and field. This is 
 a null-terminated ASCII string. Binary cursors will not
 work. 
 
-B<$length = $result-&gtgetlength($tup_num, $field_num)>
+    $length = $result->getlength($tup_num, $field_num)
 
 Returns the length of the value for a given tuple and field. 
 
-B<$null_status = $result-&gtgetisnull($tup_num, $field_num)>
+    $null_status = $result->getisnull($tup_num, $field_num)
 
 Returns the NULL status for a given tuple and field. 
 
-B<$result_status = $result-&gtresultStatus>
+    $result_status = $result->resultStatus
 
 Returns the status of the result. For comparing the status you 
 may use one of the following constants depending upon the 
 command executed:
- - PGRES_EMPTY_QUERY
- - PGRES_COMMAND_OK
- - PGRES_TUPLES_OK
- - PGRES_COPY_OUT
- - PGRES_COPY_IN
- - PGRES_BAD_RESPONSE
- - PGRES_NONFATAL_ERROR
- - PGRES_FATAL_ERROR
 
-B<$cmdStatus = $result-&gtcmdStatus>
+  - PGRES_EMPTY_QUERY
+  - PGRES_COMMAND_OK
+  - PGRES_TUPLES_OK
+  - PGRES_COPY_OUT
+  - PGRES_COPY_IN
+  - PGRES_BAD_RESPONSE
+  - PGRES_NONFATAL_ERROR
+  - PGRES_FATAL_ERROR
+
+    $cmdStatus = $result->cmdStatus
 
 Returns the command status of the last query command.
 
-B<$oid = $result-&gtoidStatus>
+    $oid = $result->oidStatus
 
 In case the last query was an INSERT command it returns the oid of the 
 inserted tuple. 
 
-B<$result-&gtprintTuples($fout, $printAttName, $terseOutput, $width)>
+    $result->printTuples($fout, $printAttName, $terseOutput, $width)
 
 Kept for backward compatibility. Use print.
 
-B<$result-&gtprint($fout, $header, $align, $standard, $html3, $expanded, $pager, $fieldSep, $tableOpt, $caption, ...)>
+    $result->print($fout, $header, $align, $standard, $html3, $expanded, $pager, $fieldSep, $tableOpt, $caption, ...)
 
 Prints out all the tuples in an intelligent  manner. This function 
 differs from the C-counterpart. The struct PQprintOpt has been 
@@ -429,7 +425,7 @@ are boolean flags. The arguments $fieldSep, $tableOpt, $caption
 are strings. You may append additional strings, which will be 
 taken as replacement for the field names. 
 
-B<PQclear($result)>
+    PQclear($result)
 
 Old style only !
 Frees all memory of the given result. 
@@ -443,57 +439,58 @@ system interface with analogues of open, close, read, write,
 lseek, tell. In order to get a consistent naming, all function 
 names have been prepended with 'PQ' (old style only). 
 
-B<$lobjId = $conn-&gtlo_creat($mode)>
+    $lobjId = $conn->lo_creat($mode)
 
 Creates a new large object. $mode is a bitmask describing 
 different attributes of the new object. Use the following constants: 
- - PGRES_INV_SMGRMASK
- - PGRES_INV_ARCHIVE
- - PGRES_INV_WRITE
- - PGRES_INV_READ
+
+  - PGRES_INV_SMGRMASK
+  - PGRES_INV_ARCHIVE
+  - PGRES_INV_WRITE
+  - PGRES_INV_READ
 
 Upon failure it returns PGRES_InvalidOid. 
 
-B<$ret = $conn-&gtlo_unlink($lobjId)>
+    $ret = $conn->lo_unlink($lobjId)
 
 Deletes a large object. Returns -1 upon failure. 
 
-B<$lobj_fd = $conn-&gtlo_open($lobjId, $mode)>
+    $lobj_fd = $conn->lo_open($lobjId, $mode)
 
 Opens an existing large object and returns an object id. 
 For the mode bits see lo_create. Returns -1 upon failure. 
 
-B<$ret = $conn-&gtlo_close($lobj_fd)>
+    $ret = $conn->lo_close($lobj_fd)
 
 Closes an existing large object. Returns 0 upon success 
 and -1 upon failure. 
 
-B<$nbytes = $conn-&gtlo_read($lobj_fd, $buf, $len)>
+    $nbytes = $conn->lo_read($lobj_fd, $buf, $len)
 
 Reads $len bytes into $buf from large object $lobj_fd. 
 Returns the number of bytes read and -1 upon failure. 
 
-B<$nbytes = $conn-&gtlo_write($lobj_fd, $buf, $len)>
+    $nbytes = $conn->lo_write($lobj_fd, $buf, $len)
 
 Writes $len bytes of $buf into the large object $lobj_fd. 
 Returns the number of bytes written and -1 upon failure. 
 
-B<$ret = $conn-&gtlo_lseek($lobj_fd, $offset, $whence)>
+    $ret = $conn->lo_lseek($lobj_fd, $offset, $whence)
 
 Change the current read or write location on the large object 
 $obj_id. Currently $whence can only be 0 (L_SET). 
 
-B<$location = $conn-&gtlo_tell($lobj_fd)>
+    $location = $conn->lo_tell($lobj_fd)
 
 Returns the current read or write location on the large object 
 $lobj_fd. 
 
-B<$lobjId = $conn-&gtlo_import($filename)>
+    $lobjId = $conn->lo_import($filename)
 
 Imports a Unix file as large object and returns 
 the object id of the new object. 
 
-B<$ret = $conn-&gtlo_export($lobjId, $filename)>
+    $ret = $conn->lo_export($lobjId, $filename)
 
 Exports a large object into a Unix file. 
 Returns -1 upon failure, 1 otherwise. 
@@ -501,7 +498,7 @@ Returns -1 upon failure, 1 otherwise.
 
 =head1 AUTHOR
 
-Edmund Mergl &ltE.Mergl@bawue.de&gt
+    Edmund Mergl <E.Mergl@bawue.de>
 
 =head1 SEE ALSO
 
